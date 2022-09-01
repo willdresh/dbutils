616ae1ea44a16867be355adb8a3fb8e40dfc6291
willdresh <w@dresh.app>
2022-09-01 18:35:23 -0500

    Added first unit test: LookupTests.Lookup_ExclusivelyOwns_ReferenceTo_Key()

--- DBInterface-XUnit-Tests/LookupTests.cs
+++ DBInterface-XUnit-Tests/LookupTests.cs
@@ -4,18 +4,41 @@
 
 using System;
 using DBInterface;
+//using Xunit; // TODO- need a reference
 
 namespace DBInterface_XUnit_Tests
 {
     internal class LookupTests
     {
         private Lookup lu;
-        private void NewLookupInstance(string newKey = null)
+
+        // TODO- need a reference
+        //[Fact]
+        public void Lookup_ExclusivelyOwns_ReferenceTo_Key()
         {
-            lu = null;
-            throw new NotImplementedException();
+            // Arrange
+            string testKey_transform = "Transform Test Key";
+            Lookup test_transform = Lookup.Build(testKey_transform);
+            string testKey_reference = "Reference Test Key";
+            Lookup test_reference = Lookup.Build(testKey_reference);
+
+            // Act
+            testKey_transform += " - Transformation Applied";
+
+            // Assert
+            //Assert.False(ReferenceEquals(testKey_reference, test_reference.KeyCopy));
+            //Assert.False(testKey_transform.Equals(test_transform.KeyCopy));
         }
 
+        private void NewLookupInstanceNullKey()
+        {
+            lu = Lookup.Build(null);
+        }
+
+        private void NewLookupInstance(string newKey)
+        {
+            lu = Lookup.Build(newKey);
+        }
 
     }
 }
