63115e3a4af4f21ff2db0dc1df73a3598b5174b5
willdresh <w@dresh.app>
2022-09-01 18:58:26 -0500

    Added unit tests for Lookup

--- DBInterface-XUnit-Tests/LookupTests.cs
+++ DBInterface-XUnit-Tests/LookupTests.cs
@@ -4,40 +4,56 @@
 
 using System;
 using DBInterface;
-//using Xunit; // TODO- need a reference
+using Xunit;
 
 namespace DBInterface_XUnit_Tests
 {
-    internal class LookupTests
+    public class LookupTests
     {
-        private Lookup lu;
-
-        // TODO- need a reference
-        //[Fact]
-        public void Lookup_ExclusivelyOwns_ReferenceTo_Key()
+        [Fact]
+        public void Build_KeyCopy_EqualTo_BuildParameter()
         {
             // Arrange
-            string testKey_transform = "Transform Test Key";
-            Lookup test_transform = Lookup.Build(testKey_transform);
-            string testKey_reference = "Reference Test Key";
-            Lookup test_reference = Lookup.Build(testKey_reference);
+            string testKey = "Build Parameter Test Key";
+
+            // Act
+            Lookup test = Lookup.Build(testKey);
+
+            // Assert
+            Assert.Equal(test.KeyCopy, testKey);
+        }
+
+        [Fact]
+        public void Equals_Instance_EqualTo_Same_Key()
+        {
+            // Arrange
+            string testKey = "Equality Test Key";
+
+            // Act
+            Lookup test1 = Lookup.Build(testKey),
+                test2 = Lookup.Build(testKey);
+
+            // Assert
+            Assert.True(test1.Equals(test2));
+        }
+
+        [Fact]
+        public void ExclusivelyOwns_ReferenceTo_Key()
+        {
+            // Arrange
+            string testKey_transform = "Transform Test Key",
+                testKey_reference = "Reference Test Key";
+            Lookup test_transform = Lookup.Build(testKey_transform),
+                test_reference = Lookup.Build(testKey_reference);
 
             // Act
             testKey_transform += " - Transformation Applied";
+            bool keysAreReferenceEqual = ReferenceEquals(testKey_reference, test_reference.KeyCopy);
+            bool ableToTransformKeyExternally = testKey_transform.Equals(test_transform.KeyCopy);
 
             // Assert
-            //Assert.False(ReferenceEquals(testKey_reference, test_reference.KeyCopy));
-            //Assert.False(testKey_transform.Equals(test_transform.KeyCopy));
-        }
-
-        private void NewLookupInstanceNullKey()
-        {
-            lu = Lookup.Build(null);
-        }
-
-        private void NewLookupInstance(string newKey)
-        {
-            lu = Lookup.Build(newKey);
+            Assert.False(keysAreReferenceEqual);
+            Assert.False(ableToTransformKeyExternally);
         }
 
     }
