f0a59cc956adae331fa6f3768f425bc103d35aed
willdresh <w@dresh.app>
2022-09-01 23:39:32 -0500

    Refactored DBLookup_Aux.cs into Exceptions.cs

--- DBInterface/Exceptions.cs
+++ DBInterface/Exceptions.cs
@@ -102,6 +102,53 @@ namespace DBInterface
         }
     }
 
+    partial class DBLookupResult
+    {
+        /// <summary>
+        /// DBLookupResult bug detected exception.
+        /// </summary>
+        /// <remarks>
+        /// These could be publicly caught as type <see cref="ArgumentNullException"/>.
+        /// </remarks>
+        internal sealed class DBLookupResult_BugDetectedException : ArgumentNullException
+        {
+            private static readonly string FBDEMessage = "A required argument was null in a call to a static internal method of DBLookupResult; this probably means a bug in the caller's code";
+            public DBLookupResult_BugDetectedException(string argumentName)
+                : base(argumentName, FBDEMessage) { }
+        }
+
+        /// <summary>
+        /// Internal instance expected exception.
+        /// </summary>
+        /// <remarks>
+        /// These could be publicly caught as type <see cref="SecurityException"/>
+        /// </remarks>
+        internal sealed class InternalInstanceExpectedException : SecurityException
+        {
+            private static readonly string IIEEMessage = "Internal DBLookupResult instance expected";
+
+            internal InternalInstanceExpectedException()
+                : base(IIEEMessage) { }
+
+            internal InternalInstanceExpectedException(string message)
+                : base(GenerateMessage(message)) { }
+
+            private static string GenerateMessage(string msg)
+            {
+                return String.Format("{0}: {1}", IIEEMessage, msg);
+            }
+        }
+    }
+    partial class DBLookup
+    {
+        internal sealed class DBLookupBugDetectedException : ArgumentNullException
+        {
+            private static readonly string FBDEMessage = "A required argument was null in a call to a static internal method of DBLookup; this may mean a bug in the caller's code";
+            public DBLookupBugDetectedException(string argumentName)
+                : base(argumentName, FBDEMessage) { }
+        }
+    }
+
     partial class DBLookupManager
     {
         internal sealed class DBLookupManagerBugDetectedException : ArgumentNullException
